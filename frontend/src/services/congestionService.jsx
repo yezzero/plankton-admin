aca700df3986a260f03b25ef4e529173a31581a5
YenaLey <yena.e121@gmail.com>
2024-11-09 23:51:07 +0900

    Feat: 공지 보내기, 사고 조회하기 api 추가

--- frontend/src/services/congestionService.jsx
+++
@@ -1,7 +0,0 @@
-import { fetchAPI } from "@/utils/fetch_api";
-
-export const getCongestionData = async (hotspotName) => {
-  const encodedHotspot = encodeURIComponent(hotspotName);
-  const endpoint = `/status/?location=${encodedHotspot}`;
-  return await fetchAPI("post", endpoint);
-};
